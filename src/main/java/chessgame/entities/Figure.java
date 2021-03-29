f07a94f164650dd8eb46fb58d8f6bf7f9a48f594
Vladlen Naumov <naumov.vlad04@gmail.com>
2021-03-29 16:51:52 +0300

    Gradle project is configured

--- src/main/java/chessgame/entities/Figure.java
+++ src/main/java/chessgame/entities/Figure.java
@@ -1,3 +1,5 @@
+package chessgame.entities;
+
 public class Figure
 {
   public int coordination_x;
---
+++ src/main/java/chessgame/entities/Pole.java
@@ -0,0 +1,37 @@
+package chessgame.entities;
+
+import  javax.swing.*;
+import java.awt.*;
+
+public class Pole  extends JFrame{
+    public JButton [][] cage= new JButton[9][9];
+
+    public Pole() {
+        super("Game");
+        for (int i=0;i<8;i++) {
+            for (int j=0;j<8;j++) {
+                cage[i][j]= new JButton();
+                cage[i][j].setPreferredSize(new Dimension(100,100));
+                if ((i + j) % 2 == 0) {
+                    cage[i][j].setBackground(Color.MAGENTA);
+                }
+                else {
+                    cage[i][j].setBackground(Color.BLUE);
+                }
+            }
+        }
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+
+        Container pane = this.getContentPane();
+        GridLayout layout =new GridLayout(8,8,0,0);
+        pane.setLayout(layout);
+         for (int i=0;i<8;i++) {
+            for (int j=0;j<8;j++) {
+                pane.add(cage[i][j]);
+            }
+        }
+        pack();
+
+    }
+}
